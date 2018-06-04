636db2b8124d2dddbf722bb2a8a8597720fe9876
Jacob Ford <j@cobford.com>
2018-06-03 23:09:40 -0400

    Flatten block structure

--- app.js
+++ app.js
@@ -12,17 +12,14 @@ let transactions = m({
 })
 
 let ore = m({
-  currentBlock: {
-    version: 536870912,
-    previousblockhash: '00000000000000000061abcd4f51d81ddba5498cff67fed44b287de0990b7266',
-    merkleroot: '871148c57dad60c0cde483233b099daa3e6492a91c13b337a5413a4c4f842978',
-    time: 1515252561,
-    bits: '180091c1'
-  },
+  version: 536870912,
+  previousblockhash: '00000000000000000061abcd4f51d81ddba5498cff67fed44b287de0990b7266',
+  merkleroot: '871148c57dad60c0cde483233b099daa3e6492a91c13b337a5413a4c4f842978',
+  hash: undefined,
+  time: undefined,
+  bits: '180091c1',
   // target
-  // hash
-  nonce: 45191988 // will be 45291998
-  // time?
+  nonce: 0 // will be 45291998
 })
 
 // MINE
